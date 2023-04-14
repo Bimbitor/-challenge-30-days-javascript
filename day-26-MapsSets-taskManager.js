52b9aae273bbb00083d955daf1f92fb4d76080d0
Bimbitor <javijohanro30@gmail.com>
2023-04-14 00:00:58 -0500

    día 26 finished

--- day-26-MapsSets-taskManager.js
+++ day-26-MapsSets-taskManager.js
@@ -1,3 +1,35 @@
 function taskManager() {
     // Tu código aquí 👈
+    const tasks = new Map();
+
+    return {
+        addTask(task, tags){
+            task = task.toLowerCase();
+            let newTags = tags
+            if(tasks.has(task)){
+                newTags = tasks.get(task)
+                tags.forEach(tag => {
+                    newTags.add(tag)
+                });
+            }
+            tasks.set(task, new Set(newTags));
+            return tasks
+        },
+
+        printTasks(){
+            console.log(tasks);
+            return tasks
+        }
+    }
 }
+
+const myTaskManager = taskManager()
+myTaskManager.addTask("Comprar leche", ["compras", "urgente"]);
+myTaskManager.addTask("Sacar al perro", ["mascotas"]);
+
+myTaskManager.printTasks();
+
+myTaskManager.addTask("Comprar leche", ["urgente", "salud"]);
+
+myTaskManager.printTasks();
+
